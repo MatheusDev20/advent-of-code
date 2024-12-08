859652d080dd79f81e410525d8d469f84c039133
matheus <matheusdev20@gmail.com>
2024-12-08 18:32:55 -0300

    day 1 part two

--- 2024/js/day1/index.js
+++ 2024/js/day1/index.js
@@ -7,6 +7,22 @@ const lines = input.replace(/\r/g, "").split("\n").filter(x => x != "").map((x) 
 const l1 = lines.map(x => x[0]).sort((a, b) => a - b)
 const l2 = lines.map(x => x[1]).sort((a, b) => a - b)
 
-const total = l1.reduce((acc, el, index) => acc + Math.abs(el - l2[index]), 0)
+const r = (l1, l2) => 
+  l1.length === 0 || l2.length === 0
+    ? 0
+    : Math.abs(l1[0] - l2[0]) + r(l1.slice(1), l2.slice(1));
 
-console.log("Total", total)
+console.log("Distance", r(l1,l2))
+
+// Part Two
+let similarity = []
+for(let i = 0; i < l1.length; i++) {
+  let ocurrence = 0;
+  for(let j = 0; j < l2.length; j++) 
+    if(l1[i] === l2[j]) ocurrence++
+
+  similarity.push({n: l1[i], ocurrence})
+}
+
+const total = similarity.reduce((acc, el) => acc + (el["n"] * el["ocurrence"]) , 0)
+console.log("Similarity", total)
