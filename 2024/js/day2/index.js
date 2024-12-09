26a24d189f51a9632f4b2115147d16d0cb153100
matheus <matheusdev20@gmail.com>
2024-12-09 11:59:48 -0300

    day 2 parto two

--- 2024/js/day2/index.js
+++ 2024/js/day2/index.js
@@ -4,13 +4,11 @@ const input = fs.readFileSync(path.resolve(__dirname, "input.txt"), "utf8");
 
 const levels = input.replace(/\r/g, "").split("\n").map(x => x.split(' ').map(Number)) 
 
-// [
-//   60, 59, 58, 55,
-//   53, 51, 48, 46
-// ],
+
 const isSafeReport = (report) => {
   const firstComparison = report[0] - report[1] 
   if(Math.abs(firstComparison) < 1 || Math.abs(firstComparison) > 3) return false
+
   const direction = firstComparison > 0 ? "decrease" : "increase"
 
   for(let i = 1; i < report.length -1; i++) {
@@ -24,6 +22,20 @@ const isSafeReport = (report) => {
 
   return true
 }
+function tolerabeReport(report) {
+	if (isSafeReport(report)) return true;
 
-const safeReports = levels.filter(isSafeReport);
-console.log("Safe reports:", safeReports.length);
+	for (let i = 0; i < report.length; i++) {
+		const trimmedReport = report.slice();
+		trimmedReport.splice(i, 1);
+		if (isSafeReport(trimmedReport)) return true;
+	}
+
+	return false;
+}
+tolerabeReport([60, 59, 58, 55, 53, 51, 48, 46])
+// const safeReports = levels.filter(tolerabeReport);
+// console.log("Safe reports:", safeReports.length);
+
+const arr = [1,2,3,4]
+console.log(arr.splice(1,1))
